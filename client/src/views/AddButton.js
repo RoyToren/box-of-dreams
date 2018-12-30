3a0a2d1fd70cfd4783756d82aec05c5e4665a83d
roy <sajezz@gmail.com>
2018-12-30 10:53:14 +0200

    changing components structure  + adding fields to card

--- client/src/views/AddButton.js
+++ client/src/views/AddButton.js
@@ -3,21 +3,21 @@ import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 
 const fab = {
-    position: 'fixed',
-    right: '0',
-    bottom: '0',
+  position: 'fixed',
+  right: '0',
+  bottom: '0',
 };
 
 class AddButton extends Component {
   render() {
-  return (
-    <div>
-      <Fab color="primary" onClick={this.props.handleClick} aria-label="Add" className="fab" style={{fab}}>
-        <AddIcon />
-      </Fab>
-    </div>
-  );
+    return (
+      <div>
+        <Fab color="primary" onClick={this.props.handleClick} aria-label="Add" className="fab" style={{fab}}>
+          <AddIcon />
+        </Fab>
+      </div>
+    );
   }
 }
 
-export default AddButton
+export default AddButton;
